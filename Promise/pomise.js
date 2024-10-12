501e7a8dbfa37cc1df1b87445dc2f0482385e0e8
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2024-10-12 18:07:52 +0300

    add task

--- Promise/pomise.js
+++ Promise/pomise.js
@@ -1,7 +1,24 @@
-// sleep
-const sleep = time =>
-	new Promise(resolve => setTimeout(resolve('hello '), time))
+//  sleep
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
-// В этом случае выполниться только первый resolve и в результате будет номер таймера
+const sleepAsync = async time => {
+	setTimeout(() => {}, time)
+}
+
+//  В этом случае выполниться только первый resolve и в результате будет номер таймера
 const sleep2 = time =>
 	new Promise(resolve => resolve(setTimeout(() => resolve('hello'), time)))
+
+//  rejectAfterSleep
+const rejectAfterSleep = (time, err) =>
+	new Promise((_, reject) => {
+		setTimeout(() => reject(err), time)
+	})
+
+// timeout
+const timeout = (action, time) => {
+	return Promise.race([
+		action,
+		setTimeout(() => Promise.reject(new Error('Timeout')), time),
+	])
+}
